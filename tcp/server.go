335bf76c27145d8ebbd6c8d929716d55d969f79e
Zili Xiao <472263998@qq.com>
2024-03-18 11:16:36 +0800

    coding

--- tcp/server.go
+++ tcp/server.go
@@ -5,6 +5,10 @@ import (
 	"go-redis/interface/tcp"
 	"go-redis/lib/logger"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 type Config struct {
@@ -13,7 +17,19 @@ type Config struct {
 
 func ListenAndServeWithSignal(cfg *Config, handler tcp.Handler) error {
 	listener, err := net.Listen("tcp", cfg.Address)
+
 	closeChan := make(chan struct{})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		sig := <-sigChan
+		switch sig {
+		case syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			closeChan <- struct{}{}
+		}
+	}()
+
 	if err != nil {
 		return err
 	}
@@ -23,15 +39,32 @@ func ListenAndServeWithSignal(cfg *Config, handler tcp.Handler) error {
 }
 
 func ListenAndServe(listener net.Listener, handler tcp.Handler, closeChan <-chan struct{}) {
+	go func() {
+		<-closeChan
+		logger.Info("shutting down")
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
+
+	defer func() {
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
 	ctx := context.Background()
+	var waitDone sync.WaitGroup
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			break
 		}
 		logger.Info("accepted link")
+		waitDone.Add(1)
 		go func() {
+			defer func() {
+				waitDone.Done()
+			}()
 			handler.Handle(ctx, conn)
 		}()
 	}
+	waitDone.Wait()
 }
